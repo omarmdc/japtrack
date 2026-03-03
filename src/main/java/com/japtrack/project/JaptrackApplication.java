49ee6a5bf5792942648d27b5901e609a5e7716fb
Omar Acosta <omar.acosta005@mymdc.net>
2026-03-02 23:55:35 -0500

    Test in new device

--- src/main/java/com/japtrack/project/JaptrackApplication.java
+++ src/main/java/com/japtrack/project/JaptrackApplication.java
@@ -9,7 +9,7 @@ public class JaptrackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JaptrackApplication.class, args);
 
-        System.out.println("Testing Japtrack");
+        System.out.println("Testing Japtrack on windows device");
 	}
 
 }
