1d15d9c71812d38f99920272fa7398340ab500c2
Omar Acosta <omar.acosta005@mymdc.net>
2026-03-07 03:11:38 -0500

    Add method signatures to UserService interface

--- src/main/java/com/japtrack/project/service/ApplicationService.java
+++ src/main/java/com/japtrack/project/service/ApplicationService.java
@@ -1,4 +1,7 @@
 package com.japtrack.project.service;
 
+import com.japtrack.project.dto.request.UserRequest;
+import com.japtrack.project.dto.response.UserResponse;
+
 public interface ApplicationService {
 }
--- src/main/java/com/japtrack/project/service/UserService.java
+++ src/main/java/com/japtrack/project/service/UserService.java
@@ -1,4 +1,13 @@
 package com.japtrack.project.service;
 
+import com.japtrack.project.dto.request.UserRequest;
+import com.japtrack.project.dto.response.UserResponse;
+
 public interface UserService {
+
+    UserResponse createUser (UserRequest request);
+    UserResponse updateUser (Long userId, UserRequest request);
+    String deleteUser (Long userId);
+    UserResponse getUserById (Long userId);
+
 }
