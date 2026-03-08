f812b3dd5fe2325cd9af7407c17f4b70a7e74430
omarmdc <omar.acosta005@mymdc.net>
2026-03-08 16:49:47 -0400

    Add UserController with CRUD endpoints
    - Map POST, GET, PATCH, DELETE for /api/users

--- src/main/java/com/japtrack/project/controller/UserController.java
+++ src/main/java/com/japtrack/project/controller/UserController.java
@@ -4,27 +4,40 @@ import com.japtrack.project.dto.request.UserRequest;
 import com.japtrack.project.dto.response.UserResponse;
 import com.japtrack.project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
 
-    // 1) Creating a user
+    // 1) Create a user
     @PostMapping
-    public UserResponse createUser(@RequestBody UserRequest request) {
-        return userService.createUser(request);
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest request) {
+        UserResponse response = userService.createUser(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    // 2)
+    // 2) Update a user
+    @PatchMapping("/{userId}")
+    public UserResponse updateUser(@PathVariable Long userId, @RequestBody UserRequest request) {
+        return userService.updateUser(userId, request);
+    }
 
+    // 3) Delete a user
+    @DeleteMapping("/{userId}")
+    public String deleteUser (@PathVariable Long userId) {
+        return userService.deleteUser(userId);
+    }
 
-
+    // 4) Get a user by id
+    @GetMapping("/{userId}")
+    public UserResponse getUserById (@PathVariable Long userId) {
+        return userService.getUserById(userId);
+    }
 }
