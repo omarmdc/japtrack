213d1e82e07c59ae4afee2b4311b4246fad80a1a
Omar Acosta <omar.acosta005@mymdc.net>
2026-03-11 01:51:50 -0400

    Add ApplicationController with createApplication endpoint

--- src/main/java/com/japtrack/project/controller/ApplicationController.java
+++ src/main/java/com/japtrack/project/controller/ApplicationController.java
@@ -1,4 +1,31 @@
 package com.japtrack.project.controller;
 
+import com.japtrack.project.dto.request.ApplicationRequest;
+import com.japtrack.project.dto.response.ApplicationResponse;
+import com.japtrack.project.service.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/applications")
 public class ApplicationController {
+
+    @Autowired
+    private ApplicationService applicationService;
+
+    @PostMapping
+    public ResponseEntity<ApplicationResponse> createApplication(@RequestBody ApplicationRequest request) {
+        ApplicationResponse response = applicationService.createApplication(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+
+
+
 }
